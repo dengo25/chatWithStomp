6d2c423ec1efee5d0066e81cb5f8442f5531f9a9
dengo25 <dengo25.dev@gmail.com>
2025-10-07 13:48:10 +0900

    [feat] 이전 메세지 조회 컨트롤러

--- src/main/java/org/dengo/chat_backend/chat/controller/ChatController.java
+++ src/main/java/org/dengo/chat_backend/chat/controller/ChatController.java
@@ -1,13 +1,13 @@
 package org.dengo.chat_backend.chat.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.dengo.chat_backend.chat.dto.ChatMessageDTO;
 import org.dengo.chat_backend.chat.service.ChatService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/chat")
@@ -20,5 +20,12 @@ public class ChatController {
   public ResponseEntity<?> getOrCreatePrivateRoom(@RequestParam Long otherMemberId) {
     Long roomId = chatService.getOrCreatePrivateRoom(otherMemberId);
     return new ResponseEntity<>(roomId, HttpStatus.OK);
+  }
+  
+  //이전 메세지 조회
+  @GetMapping("/history/{roomId}")
+  public ResponseEntity<?> getHistory(@PathVariable Long roomId) {
+    List<ChatMessageDTO> chatMessageDTOS = chatService.getHistory(roomId);
+    return new ResponseEntity<>(chatMessageDTOS, HttpStatus.OK);
   }
 }
--- src/main/java/org/dengo/chat_backend/chat/repository/ChatMessageRepository.java
+++ src/main/java/org/dengo/chat_backend/chat/repository/ChatMessageRepository.java
@@ -1,8 +1,12 @@
 package org.dengo.chat_backend.chat.repository;
 
 import org.dengo.chat_backend.chat.domain.ChatMessage;
+import org.dengo.chat_backend.chat.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+import java.util.List;
 
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+  
+  List<ChatMessage> findByChatRoomOrderByCreatedTimeAsc(ChatRoom chatRoom);
 }
